5d92ea46f8de826bc5e93283c1abac7d948c3904
karthick <prog@ecomsense.in>
2023-02-28 17:42:32 +0530

    server

--- angel_copier/server.py
+++ angel_copier/server.py
@@ -1,19 +1,38 @@
+from toolkit.fileutils import Fileutils
+from user import User
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from toolkit.logger import Logger
 import inspect
-from user import load_all_users
 import pandas as pd
-from tests.big import test_trades as big
+# from tests.big import test_trades as big
 
 logging = Logger(20)  # 2nd param 'logfile.log'
-fpath = '../../../confid/bypass_multi.xlsx'
+fpath = '../../../../confid/ketan_users.xls'
+futil = Fileutils()
+
+
+def load_all_users(fpath=fpath):
+    lst_truth = [True, 'y', 'Y', 'Yes', 'yes', 'YES']
+    users = futil.xls_to_dict(fpath)
+    obj_ldr, objs_usr = None, {}
+    for u in users:
+        is_disabled = u.get('disabled', False)
+        if is_disabled not in lst_truth:
+            au = User(**u)
+            au.auth()
+            if not au._disabled:
+                if obj_ldr:
+                    objs_usr[au._userid] = au
+                else:
+                    obj_ldr = au
+    return obj_ldr, objs_usr
+
 
 # get leader and followers instance
-obj_ldr, objs_usr = load_all_users(fpath)
-
+obj_ldr, objs_usr = load_all_users()
 
 pages = ['home']
 app = FastAPI()
@@ -28,46 +47,66 @@ async def home(request: Request):
 
 
 @ app.get("/", response_class=HTMLResponse)
-async def users(request: Request, lst_sort_col: str = None):
-    lst = []
-    for u in objs_usr:
-        # lst.append(users[u]._broker.profile)
-        lst.append(vars(objs_usr[u]))
-    if not lst:
-        lst = [{'message': 'no data'}]
-    df = pd.DataFrame(
-        data=lst,
-        columns=lst[0].keys()
-    )
-    df.set_index(['_userid'], inplace=True)
-    df.drop(columns=['_broker', '_enctoken',
-            '_totp', '_password'], inplace=True)
-    if lst_sort_col:
-        df.sort_values(by=[lst_sort_col], ascending=True, inplace=True)
-    ctx = {"request": request, "title": inspect.stack()[0][3], 'pages': pages,
-           "data": df.to_html()}
-    return jt.TemplateResponse("pandas.html", ctx)
-
-
-@ app.get("/positionbook/{user_id}", response_class=HTMLResponse)
-async def positions(request: Request, user_id):
+async def users(request: Request):
     ctx = {"request": request, "title": inspect.stack()[0][3], 'pages': pages}
-    ctx['tx'] = ['message']
-    ctx['data'] = ['no data']
+    ctx['th'] = ['message']
+    ctx['data'] = ["no data"]
     body = []
-    for u in objs_usr:
-        print(u)
-        if u.get('user_id') == user_id:
-            for f_dct in u._broker.positions:
-                k = f_dct.keys()
-                th = list(k)
-                v = f_dct.values()
-                td = list(v)
-                body.append(td)
-            if len(body) > 0:
-                ctx['th'] = th
-                ctx['data'] = body
+    for keys, row in objs_usr.items():
+        th = ['user id', 'target', 'max loss', 'disabled']
+        td = [row._userid, row._target, row._max_loss, row._disabled]
+        body.append(td)
+    if len(body) > 0:
+        ctx['th'] = th
+        ctx['data'] = body
+    return jt.TemplateResponse("users.html", ctx)
+
+
+@app.get("/orderbook/{user_id}", response_class=HTMLResponse)
+async def orders(request: Request,
+                    user_id: str = 'no data'):
+    ctx = {"request": request, "title": inspect.stack()[0][3], 'pages': pages}
+    ctx['th'] = ['message']
+    ctx['data'] = [user_id]
+    if objs_usr.get(user_id, None):
+        body = []
+        u = objs_usr[user_id]
+        pos = u._broker.orders
+        lst_pos = pos.get('data', [])
+        for f_dct in lst_pos:
+            k = f_dct.keys()
+            th = list(k)
+            v = f_dct.values()
+            td = list(v)
+            body.append(td)
+        if len(body) > 0:
+            ctx['th'] = th
+            ctx['data'] = body
     return jt.TemplateResponse("table.html", ctx)
+
+
+@app.get("/positionbook/{user_id}", response_class=HTMLResponse)
+async def positions(request: Request,
+                    user_id: str = 'no data'):
+    ctx = {"request": request, "title": inspect.stack()[0][3], 'pages': pages}
+    ctx['th'] = ['message']
+    ctx['data'] = [user_id]
+    if objs_usr.get(user_id, None):
+        body = []
+        u = objs_usr[user_id]
+        pos = u._broker.positions
+        lst_pos = pos.get('data', [])
+        for f_dct in lst_pos:
+            k = f_dct.keys()
+            th = list(k)
+            v = f_dct.values()
+            td = list(v)
+            body.append(td)
+        if len(body) > 0:
+            ctx['th'] = th
+            ctx['data'] = body
+    return jt.TemplateResponse("table.html", ctx)
+
 
 if __name__ == '__main__':
     import uvicorn
