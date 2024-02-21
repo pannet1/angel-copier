ddf9c8c1f93706bad1cfcd43775073d3ffdd4221
pannet1 <prog@ecomsense.in>
2024-02-21 10:56:48 +0530

    fix path

--- run_algo.bat
+++ run_algo.bat
@@ -1,2 +1,2 @@
 @echo on
-cmd /k "cd /d C:\ecomsense\py\env\Scripts & .\activate & cd /d C:\ecomsense\py\env\angel-copier\angel_copier & py main.py"
+cmd /k "cd /d C:\py\venv\Scripts & .\activate & cd /d C:\py\venv\angel-copier\angel_copier & py main.py"
